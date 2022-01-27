7d562902c245c59a9c3d8d8933fdcbc4411e1e3b
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-26 18:56:30 -0800

    showed username fetched from API

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { push } from 'connected-react-router';
-import { signInAction } from '../reducks/users/actions';
-
+import { signIn } from '../reducks/users/operations';
 const Login = () => {
   const dispatch = useDispatch();
 
   return (
     <div>
       <h2>Login</h2>
-      <button
-        onClick={() => {
-          dispatch(signInAction({ uid: '00001', username: 'tarahack' }));
-          dispatch(push('/'));
-        }}
-      >
-        Login
-      </button>
+      <button onClick={() => dispatch(signIn())}>Login</button>
     </div>
   );
 };
