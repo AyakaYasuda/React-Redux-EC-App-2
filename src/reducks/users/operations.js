bdf84396ab8600683706884fd846c3166807861a
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-01-31 15:48:56 -0800

    added sign up and sign in pages

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,29 +1,83 @@
 import { signInAction } from './actions';
 import { push } from 'connected-react-router';
+import { auth, db, FirebaseTimestamp } from '../../firebase';
 
-export const signIn = () => {
-  return async (dispatch, getState) => {
-    const state = getState();
-    const isSignedIn = state.users.isSignedIn;
-
-    if (!isSignedIn) {
-      const url = 'http://api.github.com/users/AyakaYasuda';
-
-      const response = await fetch(url)
-        .then(res => res.json())
-        .catch(() => null);
-
-      const username = response.login;
-
-      dispatch(
-        signInAction({
-          isSignedIn: true,
-          uid: '00001',
-          username: username,
-        })
-      );
-
-      dispatch(push('/'));
+export const signIn = (email, password) => {
+  return async dispatch => {
+    // Validation
+    if (email === '' || password === '') {
+      alert('Required items cannot be blank!');
+      return false;
     }
+
+    auth.signInWithEmailAndPassword(email, password).then(result => {
+      const user = result.user;
+
+      if (user) {
+        const uid = user.uid;
+
+        db.collection('users')
+          .doc(uid)
+          .get()
+          .then(snapshot => {
+            const data = snapshot.data();
+            dispatch(
+              signInAction({
+                isSignedIn: true,
+                role: data.role,
+                uid: uid,
+                username: data.username,
+              })
+            );
+
+            dispatch(push('/'));
+          });
+      }
+    });
+  };
+};
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async dispatch => {
+    console.log(username, email, password, confirmPassword);
+    // Validation
+    if (
+      username === '' ||
+      email === '' ||
+      password === '' ||
+      confirmPassword === ''
+    ) {
+      alert('Required items cannot be blank!');
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return false;
+    }
+
+    return auth.createUserWithEmailAndPassword(email, password).then(result => {
+      const user = result.user;
+      if (user) {
+        const uid = user.uid;
+        const timestamp = FirebaseTimestamp.now();
+
+        const userInitialData = {
+          created_at: timestamp,
+          email: email,
+          role: 'customer',
+          uid: uid,
+          // updated_at: timestamp,
+          username: username,
+        };
+
+        db.collection('users')
+          .doc(uid)
+          .set(userInitialData)
+          .then(() => {
+            dispatch(push('/'));
+          });
+      }
+    });
   };
 };
---
+++ src/templates/SignUp.jsx
@@ -0,0 +1,100 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { TextInput } from '../components/UI';
+import { PrimaryButton } from '../components/UI';
+import { signUp } from '../reducks/users/operations';
+
+const SignUp = () => {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const inputEmail = useCallback(
+    e => {
+      setEmail(e.target.value);
+    },
+    [setEmail]
+  );
+
+  const inputPassword = useCallback(
+    e => {
+      setPassword(e.target.value);
+    },
+    [setPassword]
+  );
+
+  const inputConfirmPassword = useCallback(
+    e => {
+      setConfirmPassword(e.target.value);
+    },
+    [setConfirmPassword]
+  );
+
+  const inputUsername = useCallback(
+    e => {
+      setUsername(e.target.value);
+    },
+    [setUsername]
+  );
+
+  return (
+    <div className='c-section-container'>
+      <h2 className='u-text-center u-text__headline'>Sign Up</h2>
+      <div className='module-spacer--medium' />
+      <TextInput
+        fullWidth={true}
+        label={'User name'}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={username}
+        type={'text'}
+        onChange={inputUsername}
+      />
+      <TextInput
+        fullWidth={true}
+        label={'Email'}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={email}
+        type={'email'}
+        onChange={inputEmail}
+      />
+      <TextInput
+        fullWidth={true}
+        label={'Password'}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={password}
+        type={'password'}
+        onChange={inputPassword}
+      />
+      <TextInput
+        fullWidth={true}
+        label={'Password'}
+        multiline={false}
+        required={true}
+        rows={1}
+        value={confirmPassword}
+        type={'password'}
+        onChange={inputConfirmPassword}
+      />
+      <div className='module-spacer--medium' />
+      <div className='center'>
+        <PrimaryButton
+          label={'Sign Up'}
+          onClick={() =>
+            dispatch(signUp(username, email, password, confirmPassword))
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
