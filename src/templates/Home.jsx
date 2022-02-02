9c8e5b6cf45202105f16a50b1497684e86fcbc21
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-02-01 20:10:39 -0800

    added authentication

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getUserId, getUserName } from '../reducks/users/selectors';
+import { signOut } from '../reducks/users/operations';
 
 const Home = () => {
+  const dispatch = useDispatch();
   const selector = useSelector(state => state);
   const uid = getUserId(selector);
   const userName = getUserName(selector);
@@ -12,6 +14,7 @@ const Home = () => {
       <h2>Home</h2>
       <p>user ID : {uid}</p>
       <p>user name : {userName}</p>
+      <button onClick={() => dispatch(signOut())}>SIGN OUT</button>
     </div>
   );
 };
--- src/templates/Reset.jsx
+++ src/templates/Reset.jsx
@@ -2,14 +2,13 @@ import React, { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TextInput } from '../components/UI';
 import { PrimaryButton } from '../components/UI';
-import { signIn } from '../reducks/users/operations';
+import { resetPassword } from '../reducks/users/operations';
 import { push } from 'connected-react-router';
 
-const SignIn = () => {
+const Reset = () => {
   const dispatch = useDispatch();
 
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   const inputEmail = useCallback(
     e => {
@@ -18,16 +17,9 @@ const SignIn = () => {
     [setEmail]
   );
 
-  const inputPassword = useCallback(
-    e => {
-      setPassword(e.target.value);
-    },
-    [setPassword]
-  );
-
   return (
     <div className='c-section-container'>
-      <h2 className='u-text-center u-text__headline'>Sign In</h2>
+      <h2 className='u-text-center u-text__headline'>Reset Password</h2>
       <div className='module-spacer--medium' />
       <TextInput
         fullWidth={true}
@@ -39,42 +31,23 @@ const SignIn = () => {
         type={'email'}
         onChange={inputEmail}
       />
-      <TextInput
-        fullWidth={true}
-        label={'Password'}
-        multiline={false}
-        required={true}
-        rows={1}
-        value={password}
-        type={'password'}
-        onChange={inputPassword}
-      />
       <div className='module-spacer--medium' />
       <div className='center'>
         <PrimaryButton
-          label={'Sign In'}
-          onClick={() => {
-            dispatch(signIn(email, password))
-          }}
+          label={'Reset Password'}
+          onClick={() => dispatch(resetPassword(email))}
         />
         <div className='module-spacer--medium' />
         <p
           onClick={() => {
-            dispatch(push('/signup'));
+            dispatch(push('/signin'));
           }}
         >
-          Create an acount
-        </p>
-        <p
-          onClick={() => {
-            dispatch(push('/signin/reset'));
-          }}
-        >
-          Forgot password?
+          Have an account? Log in!
         </p>
       </div>
     </div>
   );
 };
 
-export default SignIn;
+export default Reset;
